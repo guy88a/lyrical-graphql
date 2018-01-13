20902309418285012ec13711940b38763ca9caf9
unknown <guy88prog@gmail.com>
2018-01-13 17:51:46 +0200

    graphql-react interaction: S6L35-40

---
+++ client/components/SongList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+    isLoading() {
+        return this.props.data.loading;
+    }
+
+    renderSongs() {
+        return this.props.data.songs.map((song, i) => {
+            return (
+                <li key={i} data-key={i} className="collection-item">
+                    { song.title }
+                </li>
+            );
+        });
+    }
+
+    render() {
+        if(this.isLoading()) {
+            return (
+                <div>
+                    Loading Songs...
+                </div>
+            )
+        } else {
+            return (
+                <ul className="collection">
+                    {this.renderSongs()}
+                </ul>
+            )
+        }
+
+        
+    }
+}
+
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
+
+export default graphql(query)(SongList);
